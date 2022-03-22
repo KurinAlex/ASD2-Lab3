a9d5c2b83bac88c605213eb417989848eb110039
Kurin, Alex <olexkurin@gmail.com>
2022-03-22 14:02:15 +0200

    Heapsort task completed

--- ASD2-Lab3/ASD2-Lab3.cpp
+++ ASD2-Lab3/ASD2-Lab3.cpp
@@ -5,6 +5,7 @@
 
 #include "Data.h"
 #include "PriorityQueue.h"
+#include "Heapsort.h"
 
 using namespace std;
 
@@ -75,8 +76,61 @@ bool testPriorityQueue()
 	return false;
 }
 
+template<typename T>
+bool isSorted(T* l, T* r)
+{
+	for (T* i = l + 1; i != r; i++)
+	{
+		if (*i < *(i - 1))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+template <typename T>
+float testSortSpeed(void (*sort)(T*, T*), T* array, int size)
+{
+	clock_t timeStart = clock();
+	sort(array, array + size);
+	clock_t timeEnd = clock();
+	float time = (float(timeEnd - timeStart)) / CLOCKS_PER_SEC;
+	return time;
+}
+
+bool testHeapsort()
+{
+	const int size = 30000;
+	Data mySortArray[size];
+	Data stlSortArray[size];
+	for (int i = 0; i < size; i++)
+	{
+		Data randData = Data();
+		mySortArray[i] = randData;
+		stlSortArray[i] = randData;
+	}
+	float myTime = testSortSpeed(heapsort, mySortArray, size);
+	float stlTime = testSortSpeed(sort, mySortArray, size);
+	bool isDataSorted = isSorted(mySortArray, mySortArray + size);
+	cout << "My sort:" << endl;
+	cout << "Time: " << myTime << endl;
+	cout << "STL sort:" << endl;
+	cout << "Time: " << stlTime << endl << endl;
+	if (isDataSorted)
+	{
+		cout << "The lab is completed" << endl << endl;
+		return true;
+	}
+	cerr << ":(" << endl << endl;
+	return false;
+}
 
 int main()
 {
+	cout << "PriorityQueue test:" << endl;
 	testPriorityQueue();
+
+	cout << "Heapsort test:" << endl;
+	testHeapsort();
 }
